8d835c056e312f847cbe44122ca9c54860897145
vnie97 <victor4711@pm.me>
2026-05-14 17:19:27 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name="tulipan_dcf",
+      version="0.1",
+      packages=["tulipan_dcf"],
+      install_requires=["pandas==2.3.3","matplotlib==3.10.7","numpy==2.3.4"])
