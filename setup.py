ab25d45f40163436eb7b1e235f7deb8ee9af79fe
vnie97 <victor4711@pm.me>
2026-05-14 17:55:45 +0200

    Updated project files

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup
 setup(name="tulipan_dcf",
       version="0.1",
       packages=["tulipan_dcf"],
-      install_requires=["pandas==2.3.3","matplotlib==3.10.7","numpy==2.3.4"])
+      install_requires=["pandas==3.0.3","matplotlib==3.10.9","numpy==2.4.4"])
